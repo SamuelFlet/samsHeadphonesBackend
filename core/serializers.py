218b2dbd7680675642e646e24b4cab1564f7ef3b
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-03-24 12:09:18 -0400

    yup

--- core/serializers.py
+++ core/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = [ "username"]
+        fields = [ "username","id"]
         
 class UserSerializerWithToken(serializers.ModelSerializer):
 
